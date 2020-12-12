df912491bc85cb390ff32988cb68921fe9097ee2
zcftxm <1126509682@qq.com>
2020-12-12 13:51:08 +0800

    项目配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,32 +1,27 @@
 const tsImportPluginFactory = require('ts-import-plugin')
 const { getLoader } = require("react-app-rewired");
 const rewireLess = require('react-app-rewire-less');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addDecoratorsLegacy,
+  disableEsLint,
+  addBundleVisualizer,
+  addWebpackAlias,
+  adjustWorkbox 
+} = require("customize-cra");
 
-module.exports = function override(config, env) {
-    const tsLoader = getLoader(
-        config.module.rules,
-        rule =>
-            rule.loader &&
-            typeof rule.loader === 'string' &&
-            rule.loader.includes('ts-loader')
-    );
-
-    tsLoader.options = {
-        getCustomTransformers: () => ({
-            before: [ tsImportPluginFactory({
-                libraryDirectory: 'es',
-                libraryName: 'antd',
-                style: true,
-            }) ]
-        })
-    };
-
-    config = rewireLess.withLoaderOptions({
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
         javascriptEnabled: true,
         modifyVars: {
-            "@primary-color": "#1DA57A", // 主题色
-        },
-    })(config, env);
-
-    return config;
-};
+          '@primary-color': '#1DA57A'
+        }
+    })
+) 
